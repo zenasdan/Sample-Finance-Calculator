87fd7daefae4fe7cfdd1a93f6da36fe11457f2dc
Zenas Dan <zenasdan@gmail.com>
2018-02-17 17:58:54 -0800

    First Commit

---
+++ SampleFinanceCalculator/Classes/SampleFinancialCalculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleFinancialCalculator.Classes
+{
+    public class Calculator
+    {
+        public double CalculateCost(DateTime startDate, DateTime endDate)
+        {
+            List<DateTime> allDates = new List<DateTime>();
+            double itemCost = 0;
+            double totalCost = 0;
+            for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                allDates.Add(date);
+            }
+            foreach (DateTime date in allDates)
+            {
+                if (date.Day <= 7 && (date.DayOfWeek != DayOfWeek.Saturday) && (date.DayOfWeek != DayOfWeek.Sunday))
+                {
+                    itemCost = .05;
+                    totalCost += itemCost;
+                }
+                else if (date.Day > 7 && date.Day <= 14 && (date.DayOfWeek != DayOfWeek.Saturday) && (date.DayOfWeek != DayOfWeek.Sunday))
+                {
+                    itemCost = .10;
+                    totalCost += itemCost;
+                }
+                else if (date.Day > 14 && date.Day <= 21 && (date.DayOfWeek != DayOfWeek.Saturday) && (date.DayOfWeek != DayOfWeek.Sunday))
+                {
+                    itemCost = .15;
+                    totalCost += itemCost;
+                }
+                else if (date.Day > 21 && date.Day <= 28 && (date.DayOfWeek != DayOfWeek.Saturday) && (date.DayOfWeek != DayOfWeek.Sunday))
+                {
+                    itemCost = .20;
+                    totalCost += itemCost;
+                }
+                else if (date.Day > 28 && date.Day <= 31 && (date.DayOfWeek != DayOfWeek.Saturday) && (date.DayOfWeek != DayOfWeek.Sunday))
+                {
+                    itemCost = .25;
+                    totalCost += itemCost;
+                }
+
+            }
+            return totalCost;
+
+        }
+    }
+}
---
+++ SampleFinanceCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using SampleFinancialCalculator.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleFinancialCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a start and end date: ");
+            Console.WriteLine("Start Date:");
+            Console.Write("Year: ");
+            var startYear = int.Parse(Console.ReadLine());
+            Console.Write("Month: ");
+            var startMonth = int.Parse(Console.ReadLine());
+            Console.Write("Day: ");
+            var startDay = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nEnd Date:");
+            Console.Write("Year: ");
+            var endYear = int.Parse(Console.ReadLine());
+            Console.Write("Month: ");
+            var endMonth = int.Parse(Console.ReadLine());
+            Console.Write("Day: ");
+            var endDay = int.Parse(Console.ReadLine());
+
+            Calculator calcObj = new Calculator();
+
+            var totalCost = calcObj.CalculateCost(new DateTime(startYear, startMonth, startDay), new DateTime(endYear, endMonth, endDay));
+
+            Console.WriteLine($"\nThe total cost of items between these dates is: {totalCost}");
+
+            Console.ReadLine();
+        }
+    }
+}
